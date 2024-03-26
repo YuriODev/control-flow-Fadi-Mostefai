70e05777f92e0e5d990209534ee2898babb086d4
Fadi-Mostefai <fadimostefai@gmail.com>
2024-03-26 18:12:25 +0000

    done

--- exercises/exercise_12.py
+++ exercises/exercise_12.py
@@ -1,2 +1,12 @@
 # Your solution to Exercise 12
 
+def replace(num):
+    str_num = str(num)
+    str_num[0] = "*" if int(str_num[0]) % 2 == 0 else str_num[0]
+    str_num[1] = "*" if int(str_num[1]) % 2 == 0 else str_num[1]
+    str_num[2] = "*" if int(str_num[2]) % 2 == 0 else str_num[2]
+    str_num[3] = "*" if int(str_num[3]) % 2 == 0 else str_num[3]
+    return str_num
+
+num = int(input())
+print(replace(num))
